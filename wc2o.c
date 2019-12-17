21e952ee39a1ebc295e2567e7d9154399dd107ec
Robert David Graham <robert_david_graham@yahoo.com>
2019-12-17 17:01:24 -0500

    obfuscated c version

---
+++ wc2o.c
@@ -0,0 +1,25 @@
+/*
+    An Obfuscated-C version of 'wc' using state-machines
+*/
+#include <stdio.h>
+int main(void)
+{
+    static const unsigned char table[4][4] = {
+        {2,0,1,0,}, {2,0,1,0,}, {3,0,1,0,},  {3,0,1,0,}
+    };
+    static const unsigned char trans[256] = {
+        0,0,0,0,0,0,0,0,0,1,2,1,1,1,0,0,
+        0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+        1,0,
+    };
+    unsigned long counts[4] = {0,0,0,0};
+    int state = 0;
+    int c;
+
+    while ((c = getchar()) != EOF)
+        counts[state = table[state][trans[c]]]++;
+
+    printf("%lu %lu %lu\n", counts[1], counts[2], 
+                counts[0] + counts[1] + counts[2] + counts[3]);
+    return 0;
+}
